adbb2456bbfde34fd2c4c734a4ed974e9934d4d5
Dmitriy <nevermind1979@mail.ru>
2021-11-23 22:44:12 +0300

    Add Lesson_1

---
+++ src/main/java/Lesson_1/Wall.java
@@ -0,0 +1,20 @@
+package Lesson_1;
+
+public class Wall implements Obstacles {
+    private int height;
+
+    public Wall(int height) {
+        this.height = height;
+    }
+
+    public void passingThrough(Participants participants) {
+        if (participants.getSkillJump() > height && participants.isDid()) {
+            participants.jump();
+        } else {
+            participants.setDid(false);
+            System.out.println(participants.getName() + " didn't jump over!!!");
+        }
+    }
+
+
+}

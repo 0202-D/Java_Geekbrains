2a5c6fe09b2a8978eeadc06641f761b2b4b2338b
Dmitriy <nevermind1979@mail.ru>
2022-02-06 13:54:30 +0300

    Add logging

--- src/main/java/Lesson_8/ServerSide/MyServer.java
+++ src/main/java/Lesson_8/ServerSide/MyServer.java
@@ -6,7 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 /**
  * @author Dm.Petrov
  * DATE: 26.12.2021
@@ -15,21 +16,24 @@ import java.util.concurrent.Executors;
 public class MyServer {
 
     private static final Integer PORT = 8088;
-
+    private static final Logger LOGGER = LogManager.getLogger( Lesson_8.ServerSide.MyServer.class);
     private AuthService authenticationService;
     private List<ClientHandler> handlerList;
     private ExecutorService executorService;
     public MyServer() {
-        System.out.println("Server started.");
+        LOGGER.info("Server started");
+       // System.out.println("Server started.");
         executorService = Executors.newFixedThreadPool(20);
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             authenticationService = new AuthServiceImpl();
             authenticationService.start();
             handlerList = new ArrayList<>();
             while (true) {
-                System.out.println("Server wait connection...");
+             //   System.out.println("Server wait connection...");
+                LOGGER.info("Waiting client connection");
                 Socket socket = serverSocket.accept();
-                System.out.println("Client connected.");
+            //    System.out.println("Client connected.");
+                LOGGER.info("Client connected");
                 executorService.execute(  new ClientHandler(this, socket));
 
             }
@@ -49,6 +53,7 @@ public class MyServer {
 
     public synchronized void sendMessageToClients(String message) {
         handlerList.forEach(clientHandler -> clientHandler.sendMessage(message));
+        LOGGER.info(message);
     }
 
     public synchronized void sendMessageToClients(ClientHandler cH, String to, String message) {
@@ -56,6 +61,7 @@ public class MyServer {
             if (clientHandler.getNickName().equals(to)) {
                 clientHandler.sendMessage("  You have a message from  " + cH.getNickName() + ": " + message);
                 cH.sendMessage(" Message for : " + to + ": " + message);
+                LOGGER.info(message);
                 return;
             }
         }
